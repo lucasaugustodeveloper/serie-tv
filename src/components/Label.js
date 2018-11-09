8c1c1f0f777bfb6a40ff31db2ee09b8a57e030f2
EcoStage <lucas.augusto5061@gmail.com>
2018-11-09 18:09:41 -0200

    Update 2018-11-09T20:09:38.105Z

--- src/components/Label.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Label = ({ name, displayName }) => (
-    <label htmlFor={ name }>
-        { displayName }
-    </label>
-)
-
-export default Label
--- src/services/db.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-const DB = new Dexie('attraction');
-
-DB.version(1).stores({
-    attractions: '++id, name, exhibition, type'
-});
-
-export default DB;
