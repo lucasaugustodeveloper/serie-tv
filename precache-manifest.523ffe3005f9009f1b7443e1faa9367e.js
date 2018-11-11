c43c4cdd4f87e4fbe335f160f155502803fa667e
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-11-11 21:18:34 -0200

    Updates

---
+++ precache-manifest.523ffe3005f9009f1b7443e1faa9367e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "842c8076d1eadc093f3c",
+    "url": "/series-tv/static/css/main.a2700f87.chunk.css"
+  },
+  {
+    "revision": "842c8076d1eadc093f3c",
+    "url": "/series-tv/static/js/main.842c8076.chunk.js"
+  },
+  {
+    "revision": "4092a54ed7513fc714d1",
+    "url": "/series-tv/static/js/1.4092a54e.chunk.js"
+  },
+  {
+    "revision": "cdecb5e73f1e6d6cd666",
+    "url": "/series-tv/static/js/2.cdecb5e7.chunk.js"
+  },
+  {
+    "revision": "d2ff6dbb21667a0b0b9b",
+    "url": "/series-tv/static/js/3.d2ff6dbb.chunk.js"
+  },
+  {
+    "revision": "ef75ef8f526b12ebfd58",
+    "url": "/series-tv/static/css/4.fc3db2fd.chunk.css"
+  },
+  {
+    "revision": "ef75ef8f526b12ebfd58",
+    "url": "/series-tv/static/js/4.ef75ef8f.chunk.js"
+  },
+  {
+    "revision": "66f49bc76c40d2aba1dd",
+    "url": "/series-tv/static/css/5.67078958.chunk.css"
+  },
+  {
+    "revision": "66f49bc76c40d2aba1dd",
+    "url": "/series-tv/static/js/5.66f49bc7.chunk.js"
+  },
+  {
+    "revision": "ed4583e18df1d810ce72",
+    "url": "/series-tv/static/js/6.ed4583e1.chunk.js"
+  },
+  {
+    "revision": "9b6ac9053760dd628d73",
+    "url": "/series-tv/static/js/runtime~main.9b6ac905.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/series-tv/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/series-tv/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/series-tv/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/series-tv/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/series-tv/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/series-tv/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/series-tv/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/series-tv/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/series-tv/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/series-tv/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "64581907570d4448170e171ca5e80655",
+    "url": "/series-tv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c93ea1741f727d5d4d0452e7e333ef57.js"
+  "/series-tv/precache-manifest.523ffe3005f9009f1b7443e1faa9367e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/series-tv/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
